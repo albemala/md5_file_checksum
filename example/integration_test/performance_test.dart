025aa6e2e61163e4b463a69cca26b871798497c3
albemala <albemala@gmail.com>
2022-10-01 16:15:47 +0200

    updated performance tests

--- example/integration_test/performance_test.dart
+++ example/integration_test/performance_test.dart
@@ -10,36 +10,36 @@ import 'package:md5_file_checksum_example/utils/file.dart';
 void main() {
   testWidgets("performance test", (tester) async {
     final tempFile1Path = await copyAssetFileToTempDirectory(Assets.file1) ?? "";
-    _runAndMeasure(() async {
+    _runAndMeasure('File 1 native', () async {
       await Md5FileChecksum.getFileChecksum(filePath: tempFile1Path);
     });
-    _runAndMeasure(() async {
+    _runAndMeasure('File 1 Dart', () async {
       await _getFileChecksumUsingDart(filePath: tempFile1Path);
     });
 
     final tempFile2Path = await copyAssetFileToTempDirectory(Assets.file2) ?? "";
-    _runAndMeasure(() async {
+    _runAndMeasure('File 2 native', () async {
       await Md5FileChecksum.getFileChecksum(filePath: tempFile2Path);
     });
-    _runAndMeasure(() async {
+    _runAndMeasure('File 2 Dart', () async {
       await _getFileChecksumUsingDart(filePath: tempFile2Path);
     });
 
     final tempFile3Path = await copyAssetFileToTempDirectory(Assets.file3) ?? "";
-    _runAndMeasure(() async {
+    _runAndMeasure('File 3 native', () async {
       await Md5FileChecksum.getFileChecksum(filePath: tempFile3Path);
     });
-    _runAndMeasure(() async {
+    _runAndMeasure('File 3 Dart', () async {
       await _getFileChecksumUsingDart(filePath: tempFile3Path);
     });
   });
 }
 
-Future<void> _runAndMeasure(Future<void> Function() function) async {
+Future<void> _runAndMeasure(String description, Future<void> Function() function) async {
   final stopwatch = Stopwatch()..start();
   await function();
   stopwatch.stop();
-  print('${function.runtimeType} executed in ${stopwatch.elapsed.inMilliseconds}');
+  print('$description executed in ${stopwatch.elapsed.inMilliseconds}');
 }
 
 Future<String?> _getFileChecksumUsingDart({
